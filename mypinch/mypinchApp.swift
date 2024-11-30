68386de28ce9dd8668ceef06919cdf7ac5637e43
Daddy2054 <stivali-tinte-0w@icloud.com>
2024-11-30 23:11:27 +0100

    Initial Commit

---
+++ mypinch/mypinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  mypinchApp.swift
+//  mypinch
+//
+//  Created by Jean on 30/11/24.
+//
+
+import SwiftUI
+
+@main
+struct mypinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
